7b5f9649a497b2eabd499ae4d5718cddfba9e66b
Terry <b0972834939@gmail.com>
2018-03-15 22:09:16 +0800

    完整

--- WriteDocument.aspx.designer.cs
+++ WriteDocument.aspx.designer.cs
@@ -175,24 +175,6 @@ namespace WebApplication1 {
         protected global::System.Web.UI.WebControls.TextBox Txt_Text;
         
         /// <summary>
-        /// UpdatePanel2 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel2;
-        
-        /// <summary>
-        /// GridView5 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView5;
-        
-        /// <summary>
         /// txt_Proposition 控制項。
         /// </summary>
         /// <remarks>

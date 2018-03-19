a121576144ff5dc6493b8568e4f208225d4b2d46
DESKTOP-07TNNMC\AJ <as860128@gmail.com>
2018-03-19 17:08:56 +0800

    新增TG

--- SelectMemberData.aspx
+++ SelectMemberData.aspx
@@ -86,27 +86,49 @@
         </div>
     </div>
 
-
-
-
-
-
-
-
-
-    新增
-    <asp:DropDownList ID="DropDownList1" runat="server">
+      <div class="panel panel-primary">
+        <div class="panel-heading pull clickable">
+            <h3 class="panel-title "><span>新增<i class="glyphicon glyphicon-chevron-up"></i></span></h3>
+        </div>
+        <div class="panel-body">
+            <table>
+                <tr>
+                    <td class="auto-style1">
+                        <div class="panel panel-default" <%--style="width: 500px"--%>>
+                            <div class="panel-heading">
+                                <asp:DropDownList ID="DropDownList1" runat="server">
         <asp:ListItem Value="Dp">部門</asp:ListItem>
         <asp:ListItem Value="FT">公文總類</asp:ListItem>
         <asp:ListItem Value="PO">職稱</asp:ListItem>
     </asp:DropDownList>
-
-    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                                 <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
 
     <asp:Button ID="Button2" runat="server" Text="確定新增" OnClick="Button2_Click" />
 
     <asp:TextBox ID="TextBox3" runat="server" Visible="False"></asp:TextBox>
     <asp:TextBox ID="TextBox4" runat="server" Visible="False"></asp:TextBox>
-    <asp:GridView ID="TG" runat="server"></asp:GridView>
+                            </div>
+
+                            <div class="panel-body">
+                                <asp:GridView ID="TG" runat="server"></asp:GridView>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    
+    
+
+   
+   
 
 </asp:Content>
--- SelectMemberData.aspx.cs
+++ SelectMemberData.aspx.cs
@@ -140,53 +140,61 @@ namespace WebApplication1
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-
-            using (SqlConnection cn = new SqlConnection(tmpdbhelper.DB_CnStr))
+            if (!string.IsNullOrWhiteSpace(TextBox2.Text))
             {
-                cn.Open();
 
-                SqlCommand cmd2 = new SqlCommand(@"Select TID , TN from TypeGroup where Tp = @Tp ORDER BY TID DESC ");
-                cmd2.Connection = cn;
-                cmd2.Parameters.AddWithValue("@Tp", DropDownList1.SelectedValue);
-              
-                using (SqlDataReader dr = cmd2.ExecuteReader())
+
+                using (SqlConnection cn = new SqlConnection(tmpdbhelper.DB_CnStr))
                 {
-                    if (dr.Read())
+                    cn.Open();
+
+                    SqlCommand cmd2 = new SqlCommand(@"Select TID , TN from TypeGroup where Tp = @Tp ORDER BY TID DESC ");
+                    cmd2.Connection = cn;
+                    cmd2.Parameters.AddWithValue("@Tp", DropDownList1.SelectedValue);
+
+                    using (SqlDataReader dr = cmd2.ExecuteReader())
                     {
-                        TextBox3.Text = dr["TID"].ToString();
-                        TextBox4.Text = dr["TN"].ToString();
+                        if (dr.Read())
+                        {
+                            TextBox3.Text = dr["TID"].ToString();
+                            TextBox4.Text = dr["TN"].ToString();
+
+                        }
 
                     }
+                    SqlCommand cmd3 = new SqlCommand(@"select TN  from TypeGroup where TN = @TN");
+                    cmd3.Connection = cn;
+                    cmd3.Parameters.AddWithValue("@TN", TextBox2.Text);
+                    using (SqlDataReader dr = cmd3.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            Response.Write(" <script language=JavaScript> alert( '已存在 '); </script> ");
 
+                        }
+                        else
+                        {
+                            dr.Close();
+                            string sqlstr = @"Insert Into TypeGroup( TID, TN, Tp ) Values ( @TID,@TN ,@Tp) ";
+
+
+                            SqlCommand cmd = new SqlCommand(sqlstr, cn);
+                            cmd.CommandText = sqlstr;
+                            cmd.Parameters.AddWithValue("@TN", TextBox2.Text);
+                            cmd.Parameters.AddWithValue("@Tp", DropDownList1.SelectedValue);
+                            cmd.Parameters.AddWithValue("@TID", int.Parse(TextBox3.Text) + 1);
+
+                            cmd.ExecuteNonQuery();//執行命令
+
+                            Response.Write(" <script language=JavaScript> alert( '動作完成 '); </script> ");
+                            bind1();
+                        }
+                    }
                 }
-                SqlCommand cmd3 = new SqlCommand(@"select TN  from TypeGroup where TN = @TN");
-                cmd3.Connection = cn;
-                cmd3.Parameters.AddWithValue("@TN", TextBox2.Text);
-                using (SqlDataReader dr = cmd3.ExecuteReader())
-                {
-                    if(dr.Read())
-                    {
-                        Response.Write(" <script language=JavaScript> alert( '已存在 '); </script> ");
-                       
-                    }
-                    else
-                    {
-                        dr.Close();
-                         string sqlstr = @"Insert Into TypeGroup( TID, TN, Tp ) Values ( @TID,@TN ,@Tp) ";
-
-
-                        SqlCommand cmd = new SqlCommand(sqlstr, cn);
-                        cmd.CommandText = sqlstr;
-                        cmd.Parameters.AddWithValue("@TN", TextBox2.Text);
-                        cmd.Parameters.AddWithValue("@Tp", DropDownList1.SelectedValue);
-                        cmd.Parameters.AddWithValue("@TID", int.Parse(TextBox3.Text) + 1);
-
-                        cmd.ExecuteNonQuery();//執行命令
-
-                        Response.Write(" <script language=JavaScript> alert( '動作完成 '); </script> ");
-                        bind1();
-                    }
-                }             
+            }
+            else
+            {
+                Response.Write(" <script language=JavaScript> alert( '請勿空白 '); </script> ");
             }
         }
     }

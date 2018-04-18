d08b7976c5ae90d9b42242dca4a1625ea6da8a48
DESKTOP-7NVBEKS\Amd <as586100@gmail.com>
2018-04-18 14:27:02 +0800

    新增公告

---
+++ Bulletin.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GuildPage.Master" AutoEventWireup="true" CodeBehind="Bulletin.aspx.cs" Inherits="WebApplication1.Bulletin" %>
+<%@ Register assembly="CKEditor.NET" namespace="CKEditor.NET" tagprefix="CKEditor" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    &nbsp;公告標題：<asp:TextBox ID="Txt_Title" runat="server"></asp:TextBox>
+<br />
+    &nbsp;員工編號：<asp:Label ID="Lbl_EID" runat="server"></asp:Label>
+<br />
+&nbsp;公告部門：<asp:Label ID="Lbl_Dp" runat="server"></asp:Label>
+<br />
+&nbsp;公告內容：<CKEditor:CKEditorControl ID="txt_Connect" runat="server">
+</CKEditor:CKEditorControl>
+    <asp:Button ID="Btn_Save" runat="server" OnClick="Btn_Save_Click" Text="送出" />
+&nbsp; 
+</asp:Content>
---
+++ Bulletin.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Bulletin : System.Web.UI.Page
+    {
+        DbHelper tmpdbhelper = new DbHelper();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (Session["userinfo"] == null)
+                {
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    ((Label)this.Master.FindControl("Lb_Title")).Text = "新增公告";
+                    UserInfo tmpUserInfo = null;
+                    if (Session["userinfo"] is UserInfo)
+                    {
+                        tmpUserInfo = (UserInfo)Session["userinfo"];
+                        Lbl_Dp.Text = tmpUserInfo.Department;
+                        Lbl_EID.Text = tmpUserInfo.EID;
+                    }
+                }
+            }
+        }
+        protected void Btn_Save_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(tmpdbhelper.DB_CnStr))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand(@"Insert Into  Bulletin(BTitle,Context,Department,Date,EID)VALUES(@BTitle,@Context,@Department,@Date,@EID)");
+                cmd.Connection = cn;
+                cmd.Parameters.AddWithValue("@BTitle", Txt_Title.Text);
+                cmd.Parameters.AddWithValue("@Context", txt_Connect.Text);
+                cmd.Parameters.AddWithValue("@Department", Lbl_Dp.Text);
+                cmd.Parameters.AddWithValue("@Date", DateTime.Today);
+                cmd.Parameters.AddWithValue("@EID", Lbl_EID.Text);
+                cmd.ExecuteNonQuery();
+            }
+    }
+}

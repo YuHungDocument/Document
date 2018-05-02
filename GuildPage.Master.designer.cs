671ca9c64cd0280639020cee9409d4aad82b9239
DESKTOP-7NVBEKS\Amd <as586100@gmail.com>
2018-05-02 16:59:21 +0800

    後台連結

--- GuildPage.Master.designer.cs
+++ GuildPage.Master.designer.cs
@@ -130,15 +130,6 @@ namespace WebApplication1 {
         protected global::System.Web.UI.WebControls.LinkButton Lb_Set;
         
         /// <summary>
-        /// Lb_Manager 控制項。
-        /// </summary>
-        /// <remarks>
-        /// 自動產生的欄位。
-        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton Lb_Manager;
-        
-        /// <summary>
         /// LinkButton1 控制項。
         /// </summary>
         /// <remarks>
